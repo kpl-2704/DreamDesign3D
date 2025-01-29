5db31e8936de9d2ba097cfb864eaeac7415d7fa0
Kapil <kapilm@advantageclub.in>
2025-01-29 22:52:37 +0530

    server file remove

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function App() {
                 <Hero />
               </motion.div>
 
-              {/* Stats Section with Vertical Animation */}
               <motion.div
                 initial={{ opacity: 0, y: 100 }}
                 whileInView={{ opacity: 1, y: 0 }}
